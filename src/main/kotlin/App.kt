27857ab141ef76ae2f9cfcf98617c4748e6ddb3c
Mikko Suniala <mikko.suniala@iki.fi>
2022-12-11 12:29:45 +0200

    cleanup: reintroduce some emotion css usage

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,9 +1,14 @@
+import csstype.Color
+import emotion.react.css
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.h1
 
 val App = FC<Props> {
     h1 {
+        css {
+            color = Color("red")
+        }
         +"Hello, React+Kotlin/JS!"
     }
 }
