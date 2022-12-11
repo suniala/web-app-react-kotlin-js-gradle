226fb9b65dc8d26e2f3d25e54e1aab86d842108e
Mikko Suniala <mikko.suniala@iki.fi>
2022-12-11 12:23:12 +0200

    cleanup: remove excess components, libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,28 +22,14 @@ kotlin {
 }
 
 dependencies {
-    //React, React DOM + Wrappers (chapter 3)
     implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.354"))
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
 
-    //Kotlin React Emotion (CSS) (chapter 3)
     implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion")
 
-    //Video Player (chapter 7)
-    implementation(npm("react-player", "2.10.1"))
-
-    //Share Buttons (chapter 7)
-    implementation(npm("react-share", "4.4.0"))
-
-    //Coroutines & serialization (chapter 8)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-}
-
-// Heroku Deployment (chapter 9)
-tasks.register("stage") {
-    dependsOn("build")
 }
 
 rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,88 +1,9 @@
-import kotlinx.coroutines.async
-import react.*
-import react.dom.*
-import kotlinx.browser.window
-import kotlinx.coroutines.*
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
-import emotion.react.css
-import csstype.Position
-import csstype.px
+import react.FC
+import react.Props
 import react.dom.html.ReactHTML.h1
-import react.dom.html.ReactHTML.h3
-import react.dom.html.ReactHTML.div
-import react.dom.html.ReactHTML.p
-import react.dom.html.ReactHTML.img
-
-suspend fun fetchVideo(id: Int): Video {
-    val response = window
-        .fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
-        .await()
-        .text()
-        .await()
-    return Json.decodeFromString(response)
-}
-
-suspend fun fetchVideos(): List<Video> = coroutineScope {
-    (1..25).map { id ->
-        async {
-            fetchVideo(id)
-        }
-    }.awaitAll()
-}
-
-val mainScope = MainScope()
 
 val App = FC<Props> {
-    var currentVideo: Video? by useState(null)
-    var unwatchedVideos: List<Video> by useState(emptyList())
-    var watchedVideos: List<Video> by useState(emptyList())
-
-    useEffectOnce {
-        mainScope.launch {
-            unwatchedVideos = fetchVideos()
-        }
-    }
-
     h1 {
         +"Hello, React+Kotlin/JS!"
-    }
-    div {
-        h3 {
-            +"Videos to watch"
-        }
-        VideoList {
-            videos = unwatchedVideos
-            selectedVideo = currentVideo
-            onSelectVideo = { video ->
-                currentVideo = video
-            }
-        }
-
-        h3 {
-            +"Videos watched"
-        }
-        VideoList {
-            videos = watchedVideos
-            selectedVideo = currentVideo
-            onSelectVideo = { video ->
-                currentVideo = video
-            }
-        }
-    }
-    currentVideo?.let { curr ->
-        VideoPlayer {
-            video = curr
-            unwatchedVideo = curr in unwatchedVideos
-            onWatchedButtonPressed = {
-                if (video in unwatchedVideos) {
-                    unwatchedVideos = unwatchedVideos - video
-                    watchedVideos = watchedVideos + video
-                } else {
-                    watchedVideos = watchedVideos - video
-                    unwatchedVideos = unwatchedVideos + video
-                }
-            }
-        }
     }
 }
